8dc2ae25af652ee8790c761fa2a351e4d24c0476
AleksandarAleksiev <aleksandyr.j.aleksiev@gmail.com>
2025-02-07 11:39:44 +0000

    Feat: Introduce Material Design 3 Color and Theme support
    This commit introduces comprehensive support for Material Design 3's color system and theming, including light, dark, medium contrast, and high contrast color schemes. It defines color palettes and applies them through the `AppTheme` composable, and it also introduces the `AppTypography`. The navigation and the screens were removed. New files were introduced `Color.kt`, `Theme.kt` and `Type.kt` to support the colors and the theme.

--- composeApp/src/commonMain/kotlin/com/aaleksiev/compose/material/components/App.kt
+++ composeApp/src/commonMain/kotlin/com/aaleksiev/compose/material/components/App.kt
@@ -6,14 +6,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
 import com.aaleksiev.compose.material.components.di.koinConfig
-import com.aaleksiev.compose.material.components.screens.detail.DetailScreen
-import com.aaleksiev.compose.material.components.screens.list.ListScreen
 import kotlinx.serialization.Serializable
 import org.koin.compose.KoinApplication
 
@@ -30,22 +23,6 @@ fun App() {
             colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
         ) {
             Surface {
-                val navController: NavHostController = rememberNavController()
-                NavHost(navController = navController, startDestination = ListDestination) {
-                    composable<ListDestination> {
-                        ListScreen(navigateToDetails = { objectId ->
-                            navController.navigate(DetailDestination(objectId))
-                        })
-                    }
-                    composable<DetailDestination> { backStackEntry ->
-                        DetailScreen(
-                            objectId = backStackEntry.toRoute<DetailDestination>().objectId,
-                            navigateBack = {
-                                navController.popBackStack()
-                            }
-                        )
-                    }
-                }
             }
         }
     }
