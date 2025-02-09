f4e03a1a00a8ef4eb3ece09b34b9fa3a45e5e419
AleksandarAleksiev <aleksandyr.j.aleksiev@gmail.com>
2025-02-09 15:35:42 +0000

    Feat: Introduce dynamic color scheme updates in AppTheme
    This commit introduces the ability to dynamically update the color scheme in `AppTheme`.
    It defines a `MaterialColor` interface and an `AppThemeColorScheme` class that allows modifying individual colors of the color scheme.
    A `LocalMaterialColor` composition local is introduced to provide this capability to composables down the tree.
    `MaterialColorScheme` sealed interface is also introduced to support easy access and modification of material color.
    A button is added in the `App` composable to demonstrate the color update.

--- composeApp/src/commonMain/kotlin/com/aaleksiev/compose/material/components/App.kt
+++ composeApp/src/commonMain/kotlin/com/aaleksiev/compose/material/components/App.kt
@@ -1,12 +1,22 @@
 package com.aaleksiev.compose.material.components
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.aaleksiev.compose.material.components.di.koinConfig
+import com.aaleksiev.compose.material.components.theme.AppTheme
+import com.aaleksiev.compose.material.components.theme.LocalMaterialColor
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme
 import kotlinx.serialization.Serializable
 import org.koin.compose.KoinApplication
 
@@ -19,10 +29,24 @@ data class DetailDestination(val objectId: Int)
 @Composable
 fun App() {
     KoinApplication(application = koinConfig()) {
-        MaterialTheme(
-            colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme()
-        ) {
+        AppTheme {
             Surface {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    val updateMaterialColor = LocalMaterialColor.current
+                    Button(
+                        onClick = {
+                            updateMaterialColor.updateColor(
+                                colorSchemeToken = MaterialColorScheme.Primary,
+                                newColor = Color.Red
+                            )
+                        }
+                    ) {
+                        Text("Click me")
+                    }
+                }
             }
         }
     }
--- composeApp/src/commonMain/kotlin/com/aaleksiev/compose/material/components/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/aaleksiev/compose/material/components/theme/Theme.kt
@@ -1,11 +1,48 @@
 package com.aaleksiev.compose.material.components.theme
+
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.Snapshot
 import androidx.compose.ui.graphics.Color
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.Background
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.ErrorContainer
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.InverseOnSurface
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.InversePrimary
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.InverseSurface
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OnBackground
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OnError
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OnErrorContainer
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OnPrimary
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OnPrimaryContainer
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OnSecondary
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OnSecondaryContainer
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OnSurface
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OnSurfaceVariant
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OnTertiary
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OnTertiaryContainer
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.Outline
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.OutlineVariant
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.Primary
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.PrimaryContainer
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.Scrim
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.Secondary
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.SecondaryContainer
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.Surface
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.SurfaceTint
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.SurfaceVariant
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.Tertiary
+import com.aaleksiev.compose.material.components.theme.MaterialColorScheme.TertiaryContainer
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -249,18 +286,243 @@ val unspecified_scheme = ColorFamily(
 
 @Composable
 fun AppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-  val colorScheme = when {
-      darkTheme -> darkScheme
-      else -> lightScheme
-  }
-
-  MaterialTheme(
-    colorScheme = colorScheme,
-    typography = AppTypography,
-    content = content
-  )
+    val appThemeColorScheme = remember(isDarkTheme) { AppThemeColorScheme(isDarkTheme) }
+    CompositionLocalProvider(
+        LocalMaterialColor provides appThemeColorScheme
+    ) {
+        MaterialTheme(
+            colorScheme = appThemeColorScheme.appColorScheme,
+            typography = AppTypography,
+            content = content
+        )
+    }
 }
 
+interface MaterialColor {
+    fun updateColor(colorSchemeToken: MaterialColorScheme, newColor: Color)
+}
+
+val LocalMaterialColor = compositionLocalOf<MaterialColor> { AppThemeColorScheme(false) }
+
+@Immutable
+private class AppThemeColorScheme(isDarkTheme: Boolean) : MaterialColor {
+    var appColorScheme: ColorScheme by mutableStateOf(if (isDarkTheme) darkScheme else lightScheme)
+        private set
+
+    override fun updateColor(colorSchemeToken: MaterialColorScheme, newColor: Color) {
+        val updatedScheme = when (colorSchemeToken) {
+            is Primary -> appColorScheme.copy(primary = newColor)
+            is OnPrimary -> appColorScheme.copy(onPrimary = newColor)
+            is PrimaryContainer -> appColorScheme.copy(primaryContainer = newColor)
+            is OnPrimaryContainer -> appColorScheme.copy(onPrimaryContainer = newColor)
+            is InversePrimary -> appColorScheme.copy(inversePrimary = newColor)
+            is Secondary -> appColorScheme.copy(secondary = newColor)
+            is OnSecondary -> appColorScheme.copy(onSecondary = newColor)
+            is SecondaryContainer -> appColorScheme.copy(secondaryContainer = newColor)
+            is OnSecondaryContainer -> appColorScheme.copy(onSecondaryContainer = newColor)
+            is Tertiary -> appColorScheme.copy(tertiary = newColor)
+            is OnTertiary -> appColorScheme.copy(onTertiary = newColor)
+            is TertiaryContainer -> appColorScheme.copy(tertiaryContainer = newColor)
+            is OnTertiaryContainer -> appColorScheme.copy(onTertiaryContainer = newColor)
+            is Background -> appColorScheme.copy(background = newColor)
+            is OnBackground -> appColorScheme.copy(onBackground = newColor)
+            is Surface -> appColorScheme.copy(surface = newColor)
+            is OnSurface -> appColorScheme.copy(onSurface = newColor)
+            is SurfaceVariant -> appColorScheme.copy(surfaceVariant = newColor)
+            is OnSurfaceVariant -> appColorScheme.copy(onSurfaceVariant = newColor)
+            is SurfaceTint -> appColorScheme.copy(surfaceTint = newColor)
+            is InverseSurface -> appColorScheme.copy(inverseSurface = newColor)
+            is InverseOnSurface -> appColorScheme.copy(inverseOnSurface = newColor)
+            is MaterialColorScheme.Error -> appColorScheme.copy(error = newColor)
+            is OnError -> appColorScheme.copy(onError = newColor)
+            is ErrorContainer -> appColorScheme.copy(errorContainer = newColor)
+            is OnErrorContainer -> appColorScheme.copy(onErrorContainer = newColor)
+            is Outline -> appColorScheme.copy(outline = newColor)
+            is OutlineVariant -> appColorScheme.copy(outlineVariant = newColor)
+            is Scrim -> appColorScheme.copy(scrim = newColor)
+        }
+
+        Snapshot.withMutableSnapshot {
+            appColorScheme = updatedScheme
+        }
+    }
+}
+
+sealed interface MaterialColorScheme {
+    data object Primary : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.primary
+    }
+
+    data object OnPrimary : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.onPrimary
+    }
+
+    data object PrimaryContainer : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.primaryContainer
+    }
+
+    data object OnPrimaryContainer : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.onPrimaryContainer
+    }
+
+    data object InversePrimary : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.inversePrimary
+    }
+
+    data object Secondary : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.secondary
+    }
+
+    data object OnSecondary : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.onSecondary
+    }
+
+    data object SecondaryContainer : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.secondaryContainer
+    }
+
+    data object OnSecondaryContainer : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.onSecondaryContainer
+    }
+
+    data object Tertiary : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.tertiary
+    }
+
+    data object OnTertiary : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.onTertiary
+    }
+
+    data object TertiaryContainer : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.tertiaryContainer
+    }
+
+    data object OnTertiaryContainer : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.onTertiaryContainer
+    }
+
+    data object Background : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.background
+    }
+
+    data object OnBackground : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.onBackground
+    }
+
+    data object Surface : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.surface
+    }
+
+    data object OnSurface : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.onSurface
+    }
+
+    data object SurfaceVariant : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.surfaceVariant
+    }
+
+    data object OnSurfaceVariant : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.onSurfaceVariant
+    }
+
+    data object SurfaceTint : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.surfaceTint
+    }
+
+    data object InverseSurface : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.inverseSurface
+    }
+
+    data object InverseOnSurface : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.inverseOnSurface
+    }
+
+    data object Error : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.error
+    }
+
+    data object OnError : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.onError
+    }
+
+    data object ErrorContainer : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.errorContainer
+    }
+
+    data object OnErrorContainer : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.onErrorContainer
+    }
+
+    data object Outline : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.outline
+    }
+
+    data object OutlineVariant : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.outlineVariant
+    }
+
+    data object Scrim : MaterialColorScheme {
+        val color: Color
+            @Composable
+            get() = MaterialTheme.colorScheme.scrim
+    }
+}
